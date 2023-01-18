f6dee30f253b50b4e086fbb014eb7db684926c8f
Grimoireheartz <120167489+Grimoireheartz@users.noreply.github.com>
2023-01-18 17:08:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -15,24 +15,24 @@ const puppeteer = require("puppeteer");
 
     // await page.waitForSelector('body > header > div > div.menu-bar')
     await page.goto('https://toyota-isite.eu/Utilization/Index?menu=Utilization');
-    // await page.focus('#fromDate');
-    // await page.keyboard.down('Control');
-    // await page.keyboard.press('A');
-    // await page.keyboard.up('Control');
-    // await page.keyboard.press('Backspace');
-    // await page.type('#fromDate', '10/1/2022',{ delay: 50})
+    await page.focus('#fromDate');
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+    await page.type('#fromDate', '10/1/2022',{ delay: 50})
 
-    // await page.focus('#endDate');
-    // await page.keyboard.down('Control');
-    // await page.keyboard.press('A');
-    // await page.keyboard.up('Control');
-    // await page.keyboard.press('Backspace');
-    // await page.type('#endDate', '10/30/2022',{ delay: 50})
-    // await page.click('#SiteSelectorAction', {delay: 50})
-    // await page.click('#selectAllSites', {delay: 50})
-    // await page.click('.dimmedCaption', { delay: 50 })
-    // await page.click('#searchButton', { delay: 50})
-    // await page.waitForSelector('#searchResultContainer > div.search-top-container > div > div.searchResultSummary > div.total-summary');
+    await page.focus('#endDate');
+    await page.keyboard.down('Control');
+    await page.keyboard.press('A');
+    await page.keyboard.up('Control');
+    await page.keyboard.press('Backspace');
+    await page.type('#endDate', '10/30/2022',{ delay: 50})
+    await page.click('#SiteSelectorAction', {delay: 50})
+    await page.click('#selectAllSites', {delay: 50})
+    await page.click('.dimmedCaption', { delay: 50 })
+    await page.click('#searchButton', { delay: 50})
+    await page.waitForSelector('#searchResultContainer > div.search-top-container > div > div.searchResultSummary > div.total-summary');
 
 
 
@@ -43,8 +43,75 @@ const puppeteer = require("puppeteer");
     // console.log('Total=>' + text_total)
 
 
+    // let countitemflesh = 1;
+    // let countdata = 0;
+
+    // let serialMachine = ''
+    // let cusSite = ''
+    // let utilizationData = ''
+    // let contractTypeData = ''
+    // let familyData = ''
+    // let modelData = ''
+    // let lastupdateData = ''
+
+
     // await browser.close();
 
+
+
+
+
+    // await scapeinfiniscroll(page, 100)
+    // while (countdata < 100) {
+    //     countitemflesh++
+    //     for (let i = 1; i <= 20; i++) {
+
+    //         countdata++;
+
+    //         if (countdata <= 20) {
+    //             let element = await page.waitForSelector(`#resultItems > div > div:nth-child(${i}) > div.resultTitleRow.resultIconSpace.result-row-title > div.column160 > span > a`)
+    //             let text = await page.evaluate(element => element.textContent, element)
+
+    //             let element_operatiomtime = await page.waitForSelector(`#resultItems > div > div:nth-child(${i}) > div.resultTitleRow.resultIconSpace.result-row-title > div.column100 > span > a`)
+    //             let text_operatiomtime = await page.evaluate(element_operatiomtime => element_operatiomtime.textContent, element_operatiomtime)
+
+    //             let element_utilize = await page.waitForSelector(`#resultItems > div > div:nth-child(${i}) > div.resultTitleRow.resultIconSpace.result-row-title > div:nth-child(5) > span`)
+    //             let text_utilize = await page.evaluate(element_utilize => element_utilize.textContent, element_utilize)
+
+    //             let element_cussite = await page.waitForSelector(`#resultItems > div:nth-child(1) > div:nth-child(${i}) > div.resultSubRow > div:nth-child(1)`)
+    //             let text_cussite = await page.evaluate(element_cussite => element_cussite.textContent, element_cussite)
+    //             text_cussite = text_cussite.replace(/\s/g, '');
+    //             text_cussite = text_cussite.replace('Site:', '');
+
+    //             let element_contracttype = await page.waitForSelector(`#resultItems > div:nth-child(1) > div:nth-child(${i}) > div.resultSubRow > div:nth-child(4)`)
+    //             let text_contracttype = await page.evaluate(element_contracttype => element_contracttype.textContent, element_contracttype)
+    //             text_contracttype = text_contracttype.replace(/\s/g, '');
+    //             text_contracttype = text_contracttype.replace('Owner:', '');
+
+    //             let element_family = await page.waitForSelector(`#resultItems > div:nth-child(1) > div:nth-child(${i}) > div.resultSubRow > div:nth-child(5)`)
+    //             let text_family = await page.evaluate(element_family => element_family.textContent, element_family)
+    //             text_family = text_family.replace(/\s/g, '');
+    //             text_family = text_family.replace('MachineFamily:', '');
+
+    //             let element_model = await page.waitForSelector(`#resultItems > div:nth-child(1) > div:nth-child(${i}) > div.resultSubRow > div:nth-child(6)`)
+    //             let text_model = await page.evaluate(element_model => element_model.textContent, element_model)
+    //             text_model = text_model.replace(/\s/g, '');
+    //             text_model = text_model.replace('Model:', '');
+
+    //             let element_lastupdate = await page.waitForSelector(`#resultItems > div:nth-child(1) > div:nth-child(${i}) > div.resultSubRow > div:nth-child(9)`)
+    //             let text_lastupdate = await page.evaluate(element_lastupdate => element_lastupdate.textContent, element_lastupdate)
+    //             text_lastupdate = text_lastupdate.replace(/\s/g, '');
+    //             text_lastupdate = text_lastupdate.replace('LastUpdated:', '');
+
+    //             serialMachine += text + ','
+    //             cusSite += text_cussite + ','
+    //             utilizationData += text_utilize + ','
+    //             contractTypeData += text_contracttype + ','
+    //             familyData += text_family + ','
+    //             modelData += text_model + ','
+    //             lastupdateData += text_lastupdate + ','
+
+    
 })();
 
 

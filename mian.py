4c00c07837138d90da4d3ab6bed8874ba2ba251b
Grimoireheartz <120167489+Grimoireheartz@users.noreply.github.com>
2024-03-31 12:56:56 +0700

    Add files via upload

---
+++ mian.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://toyota-isite.eu")
+
+
+time.sleep(5)
+
+username_field = driver.find_element(By.XPATH, '//*[@id="input_1"]')
+username_field.send_keys("external\exsuyat1")
+
+password_field = driver.find_element(By.XPATH, '//*[@id="input_2"]')
+password_field.send_keys("Donut5821270015")
+
+password_field.send_keys(Keys.ENTER)
+
+driver.get("https://toyota-isite.eu/Utilization/Index?menu=Utilization")
+
+button = driver.find_element(By.XPATH, '//*[@id="SiteSelectorAction"]/div[1]/div[1]')
+button.click()
+
+button = driver.find_element(By.XPATH, '//*[@id="selectAllSites"]/a')
+button.click()
+
+time.sleep(5)
+
+driver.get("https://toyota-isite.eu/Diagnostics/Machines?menu=Admin")
+
+button = driver.find_element(By.XPATH, '//*[@id="searchButton"]')
+button.click()
+
+
+time.sleep(20)
+#print(driver.page_source)
+
+#driver.quit()
+

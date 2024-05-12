1fb8efe2c3187cc78768496e79460073adec7c2a
Delion.SY <120167489+Grimoireheartz@users.noreply.github.com>
2024-05-12 23:46:06 +0700

    Add files via upload

--- mian.py
+++ mian.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
@@ -23,10 +25,47 @@ driver.get("https://toyota-isite.eu/Utilization/Index?menu=Utilization")
 button = driver.find_element(By.XPATH, '//*[@id="SiteSelectorAction"]/div[1]/div[1]')
 button.click()
 
+"""
 button = driver.find_element(By.XPATH, '//*[@id="selectAllSites"]/a')
 button.click()
 
-#time.sleep(5)
+"""
+button = driver.find_element(By.XPATH, '//*[@id="ssOption-79721"]/a') #ampas
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site66911"]')#Benz_samrong
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site109505"]')#Benz_samutprakan
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site76317"]')#DHL_CRG 
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site113692"]')#DHL_DSC
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site113419"]')#DHL_LOREAL
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site103877"]')#Fuji Seal Bangpoo
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site83435"]')#GPV_Asia_Bangpoo
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site105912"]')#Nestle_Bangchan
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site81669"]')#Nestle_Bangpoo
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site109513"]')#Perfect_Saimai DC
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site91850"]')#Powerbuy_Bangna
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site105790"]')#Salee colour bangpoo
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site53274"]')#Schneider Bangpoo
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site126857"]')#Tripetch romklao
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site80873"]')#UTI DSV Bangsaothong
+button.click()
+button = driver.find_element(By.XPATH, '//*[@id="site88979"]')#Watson Ticon Bangna
+button.click()
+
+time.sleep(5)
 
 driver.get("https://toyota-isite.eu/Diagnostics/Machines?menu=Admin")
 
@@ -34,10 +73,10 @@ button = driver.find_element(By.XPATH, '//*[@id="searchButton"]')
 button.click()
 
 
-time.sleep(40)
+time.sleep(45)
 body = driver.find_element(By.TAG_NAME, 'body')
-body.send_keys(Keys.PAGE_DOWN)
-index = 1 
+#body.send_keys(Keys.PAGE_DOWN)
+index = 0 
 time.sleep(3)
 
 #while True:
@@ -50,33 +89,42 @@ for i in range(10):
     xpath_expression = '//*[@id="resultItems"]/div[1]/div[{}]/div[1]/div[8]/a'.format(index + 1)
 
     errors = driver.find_elements(By.XPATH, xpath_expression)
-    
+
     print("INDEX = ", index)
     for error in errors:
         print("Errors =", error.text)
     
-    # Check if the 'errors' list is empty
     if len(errors) == 0:
         continue
 
-    # Process the 'errors' list if it's not empty
     for error in errors: 
         if int(error.text) > 0:
             ActionChains(driver).context_click(error).perform()
-            ActionChains(driver).send_keys('t').perform()
-    
+            driver.execute_script("window.open(arguments[0], '_blank');", error.get_attribute("href"))
+            time.sleep(3)
+            driver.switch_to.window(driver.window_handles[-1])
+            time.sleep(5)
+
+            svg_element = driver.find_element(By.CSS_SELECTOR, "#reportGenerator > svg")
+            actions = ActionChains(driver)
+            actions.move_to_element(svg_element).click().perform()
+
+            time.sleep(10)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            body.send_keys(Keys.PAGE_DOWN)
+         
+
     index += 1
 
     time.sleep(3)
 
-      # ตรวจสอบว่ามีการเลื่อนหน้าจอขึ้นไปหรือไม่
     scrolled = False
     while not scrolled:
         try:
             body.send_keys(Keys.PAGE_DOWN)
             scrolled = True
         except Exception as e:
-            # หากเกิดข้อผิดพลาดระหว่างการเลื่อนหน้าจอ รอสักครู่แล้วลองอีกครั้ง
             print("Error occurred while scrolling:", e)
             time.sleep(1)
 

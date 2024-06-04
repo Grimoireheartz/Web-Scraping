19867744c0882d1552b9a103dc593f3d75464e1e
Delion.SY <120167489+Grimoireheartz@users.noreply.github.com>
2024-06-04 09:30:05 +0700

    Add files via upload

--- second.py
+++ second.py
@@ -10,8 +10,6 @@ driver = webdriver.Chrome()
 
 driver.get("https://toyota-isite.eu")
 
-#time.sleep(5)
-
 username_field = driver.find_element(By.XPATH, '//*[@id="input_1"]')
 username_field.send_keys("external\exsuyat1")
 
@@ -25,112 +23,102 @@ driver.get("https://toyota-isite.eu/Utilization/Index?menu=Utilization")
 button = driver.find_element(By.XPATH, '//*[@id="SiteSelectorAction"]/div[1]/div[1]')
 button.click()
 
-"""
-button = driver.find_element(By.XPATH, '//*[@id="selectAllSites"]/a')
-button.click()
+# Select multiple sites
+sites = [
+    '//*[@id="ssOption-79721"]/a', #ampas
+    '//*[@id="site66911"]', #Benz_samrong
+    '//*[@id="site109505"]', #Benz_samutprakan
+    '//*[@id="site76317"]', #DHL_CRG 
+    '//*[@id="site113692"]', #DHL_DSC
+    '//*[@id="site113419"]', #DHL_LOREAL
+    '//*[@id="site103877"]', #Fuji Seal Bangpoo
+    '//*[@id="site83435"]', #GPV_Asia_Bangpoo
+    '//*[@id="site105912"]', #Nestle_Bangchan
+    '//*[@id="site81669"]', #Nestle_Bangpoo
+    '//*[@id="site109513"]', #Perfect_Saimai DC
+    '//*[@id="site91850"]', #Powerbuy_Bangna
+    '//*[@id="site105790"]', #Salee colour bangpoo
+    '//*[@id="site53274"]', #Schneider Bangpoo
+    '//*[@id="site126857"]', #Tripetch romklao
+    '//*[@id="site80873"]', #UTI DSV Bangsaothong
+    '//*[@id="site88979"]', #Watson Ticon Bangna
+]
 
-"""
-button = driver.find_element(By.XPATH, '//*[@id="ssOption-79721"]/a') #ampas
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site66911"]')#Benz_samrong
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site109505"]')#Benz_samutprakan
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site76317"]')#DHL_CRG 
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site113692"]')#DHL_DSC
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site113419"]')#DHL_LOREAL
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site103877"]')#Fuji Seal Bangpoo
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site83435"]')#GPV_Asia_Bangpoo
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site105912"]')#Nestle_Bangchan
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site81669"]')#Nestle_Bangpoo
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site109513"]')#Perfect_Saimai DC
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site91850"]')#Powerbuy_Bangna
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site105790"]')#Salee colour bangpoo
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site53274"]')#Schneider Bangpoo
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site126857"]')#Tripetch romklao
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site80873"]')#UTI DSV Bangsaothong
-button.click()
-button = driver.find_element(By.XPATH, '//*[@id="site88979"]')#Watson Ticon Bangna
-button.click()
+for site in sites:
+    button = driver.find_element(By.XPATH, site)
+    button.click()
 
 time.sleep(5)
 
 driver.get("https://toyota-isite.eu/Diagnostics/Machines?menu=Admin")
 
-
 button = driver.find_element(By.XPATH, '//*[@id="searchButton"]')
 button.click()
 time.sleep(45)
 
-
 body = driver.find_element(By.TAG_NAME, 'body')
-index = 0 
+outer_index = 1
+inner_index = 1
 time.sleep(3)
 
 while True:
-
     body.send_keys(Keys.PAGE_DOWN)
-
     print("enter while loop !")
 
-    xpath_expression = '//*[@id="resultItems"]/div[1]/div[{}]/div[1]/div[8]/a'.format(index + 1)
-    row_expression = '//*[@id="resultItems"]/div[1]/div[{}]/div[1]'.format(index + 1)
+    xpath_expression = f'//*[@id="resultItems"]/div[{outer_index}]/div[{inner_index}]/div[1]/div[8]/a'
+    row_expression = f'//*[@id="resultItems"]/div[{outer_index}]/div[{inner_index}]/div[1]'
 
     errors = driver.find_elements(By.XPATH, xpath_expression)
-    row_elements   = driver.find_elements(By.XPATH, row_expression)
+    row_elements = driver.find_elements(By.XPATH, row_expression)
 
-    print("INDEX = ", index)
+    print("OUTER_INDEX =", outer_index, "INNER_INDEX =", inner_index)
 
+    if not row_elements:
+        # If no row elements found, assume we reached the end
+        break
 
     row_style = row_elements[0]
     
     if row_style.get_attribute("style") == "display: none;" or not row_style.is_displayed() or row_style.get_attribute("style").find("opacity: 0.4") != -1:
-        print(f"Row {index + 1} has been removed or hidden.")
-        index += 1
+        print(f"Row {outer_index}-{inner_index} has been removed or hidden.")
+        inner_index += 1
+        if inner_index > 50:
+            outer_index += 1
+            inner_index = 1
         continue
-   
 
     for error in errors:
         print("Errors =", error.text)
-    
+
     if len(errors) == 0:
+        inner_index += 1
+        if inner_index > 50:
+            outer_index += 1
+            inner_index = 1
         continue
 
-    for error in errors: 
+    for error in errors:
         if int(error.text) > 0:
-                ActionChains(driver).context_click(error).perform()
-                driver.execute_script("window.open(arguments[0], '_blank');", error.get_attribute("href"))
-                time.sleep(3)
-                driver.switch_to.window(driver.window_handles[-1])
-                time.sleep(3)
+            ActionChains(driver).context_click(error).perform()
+            driver.execute_script("window.open(arguments[0], '_blank');", error.get_attribute("href"))
+            time.sleep(3)
+            driver.switch_to.window(driver.window_handles[-1])
+            time.sleep(3)
 
-                driver.execute_script("window.scrollBy(0, 200);")
-                svg_element = driver.find_element(By.CSS_SELECTOR, "#reportGenerator > svg")
-                actions = ActionChains(driver)
-                actions.move_to_element(svg_element).click().perform()
+            driver.execute_script("window.scrollBy(0, 200);")
+            svg_element = driver.find_element(By.CSS_SELECTOR, "#reportGenerator > svg")
+            actions = ActionChains(driver)
+            actions.move_to_element(svg_element).click().perform()
 
-                time.sleep(10)
-                driver.close()
-                driver.switch_to.window(driver.window_handles[0])
-                    #body.send_keys(Keys.PAGE_DOWN)
-            
+            time.sleep(10)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
 
-
-    index += 1
+    inner_index += 1
+    if inner_index > 50:
+        outer_index += 1
+        inner_index = 1
 
     time.sleep(3)
-
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath_expression)))
     time.sleep(3)
